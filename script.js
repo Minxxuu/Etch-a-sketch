06e4d34362be766db2be73b39ac2d10c0cda5d30
ollie <henwoodo97@gmail.com>
2026-04-06 19:49:54 +0100

    funcitnoality for grid sizing and mouse colour over

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-let gridInput = 16;
+let gridInput = 0;
 const container = document.querySelector(".container")
+const btn = document.getElementById("gridButton")
+
+btn.addEventListener("click", () => {userInput = prompt("Enter a new grid size:"), setGridSize()});
+
 
 function grid(){
     for(let i = 0; i < gridInput; i++)
     {
         const newSquare = document.createElement("div");
+        newSquare.classList.add("Square")
         container.appendChild(newSquare)
+        newSquare.addEventListener("mouseover", (event) => {event.target.style.backgroundColor = "red"})
     }
 }
+
+function setGridSize()
+{
+    gridInput = userInput;
+    if(userInput > 100)
+    {
+        alert("Cannot exceed 100")
+    }
+    grid();
+}
+
 grid();
